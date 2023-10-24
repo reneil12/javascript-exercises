6fa2460cf037b4910ae4d21bc96c14e8db69690c
reneil12 <reneilrepollo@gmail.com>
2023-10-24 21:47:41 +0800

    Finished 03_repeatString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = '';
+    for (let i = text.length - 1; i >= 0; i--){
+        reverseText += text[i]
+    }
+    return reverseText;
 };
-
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
