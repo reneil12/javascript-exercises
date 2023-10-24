c01b1595eb6aa2093ea1643d73d392299035181d
reneil12 <reneilrepollo@gmail.com>
2023-10-24 21:33:09 +0800

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(heyString, n) {
+    let text = "";
+    if (n >= 0){
+        for (let i = 1; i <= n; i++){
+            text += heyString;
+        }
+    }
+    else {
+        text = 'ERROR';
+    }
+    return text;
 };
-
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', 190);
+repeatString('', 10);
 // Do not edit below this line
 module.exports = repeatString;
