a099bad7de1cca06537e30681920ca0f8c388d0a
reneil12 <reneilrepollo@gmail.com>
2023-10-25 00:54:53 +0800

    Finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertToC = (fahrenheit - 32) * (5/9);
+  let celsius = Math.round(convertToC * 10) / 10
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertToF = celsius * (9/5) + 32
+  let fahrenheit = Math.round(convertToF * 10) / 10
+  return fahrenheit;
 };
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
 
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
