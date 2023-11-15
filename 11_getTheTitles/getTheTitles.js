042c8e29da266e6b7d80f409535e088ac1cc567f
reneil12 <reneilrepollo@gmail.com>
2023-11-16 01:09:34 +0800

    Finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
 
+const getTheTitles = function(arr) {
+    const getTitles = arr.map((book)=> {
+        return book.title;
+    })
+    return getTitles;
 };
-
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
